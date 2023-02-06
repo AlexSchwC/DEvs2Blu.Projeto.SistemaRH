118fc441b5b52b4d2adb621bd97c9b1a32d2bd96
Alexandre Schwanke da Costa <alexandreschwc@gmail.com>
2023-02-06 12:27:25 -0300

    Add project files.

---
+++ Devs2Blu.Projeto.SistemaRH.Infra.Data/SQLServerContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devs2Blu.Projeto.SistemaRH.Infra.Data
+{
+    public class SQLServerContext : DbContext
+    {
+        public SQLServerContext(DbContextOptions<SQLServerContext> options)
+            : base(options)
+        {
+
+        }
+
+        #region DbSets
+
+
+
+        #endregion
+    }
+}
---
+++ Devs2Blu.Projeto.SistemaRH.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Devs2Blu.Projeto.SistemaRH.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
